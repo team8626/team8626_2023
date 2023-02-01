9329de8d41f2e6a0d18f256b2ce0839ead4bbee0
git_master <team8626.sw@gmail.com>
2023-02-01 17:56:54 -0500

    Create Subsystems templates and CAN ID for Swerve

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,15 +42,15 @@ public final class Constants {
         public static final double kBackRightChassisAngularOffset = Math.PI / 2;
     
         // SPARK MAX CAN IDs
-        public static final int kFrontLeftDrivingCanId = 11;
-        public static final int kRearLeftDrivingCanId = 13;
-        public static final int kFrontRightDrivingCanId = 15;
-        public static final int kRearRightDrivingCanId = 17;
+        public static final int kFrontLeftDrivingCanId = 4;
+        public static final int kRearLeftDrivingCanId = 3;
+        public static final int kFrontRightDrivingCanId = 9;
+        public static final int kRearRightDrivingCanId = 21;
     
-        public static final int kFrontLeftTurningCanId = 10;
-        public static final int kRearLeftTurningCanId = 12;
-        public static final int kFrontRightTurningCanId = 14;
-        public static final int kRearRightTurningCanId = 16;
+        public static final int kFrontLeftTurningCanId = 2;
+        public static final int kRearLeftTurningCanId = 10;
+        public static final int kFrontRightTurningCanId = 13;
+        public static final int kRearRightTurningCanId = 20;
     
         public static final boolean kGyroReversed = false;
       }
